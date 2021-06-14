222eb1714429edf2f464505392ca20a617147eb9
Kehinde <adaeza.dev@gmail.com>
2021-06-14 23:44:24 +0100

    done with post page, working currently on the comment section

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Avatar } from "@material-ui/core";
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -7,22 +8,105 @@ import defaultImage from "../Images/pad.jpg";
 
 // Data import
 import Data from "../Data/sample1";
+import commentData from "../Data/comment";
 
-const Main = ({ Title, Author, Intro }) => {
+// AddComment Component
+const AddComment = () => {
+  return (
+    <>
+      <form action="#" className="commentForm">
+        <div className="row1">
+          <input
+            type="text"
+            name="name"
+            placeholder="enter name here"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="enter valid email here"
+            required
+          />
+        </div>
+        <textarea
+          name="message"
+          cols="30"
+          rows="3"
+          placeholder="Enter your comment here"
+          required
+        ></textarea>
+        <input type="submit" value="Post comment" />
+      </form>
+    </>
+  );
+};
+
+// Comment Component
+const Comments = (props) => {
+  const Comments = props.comment;
+  return (
+    <>
+      {Comments.map((comment) => {
+        return (
+          <div key={comment.id} className="commentItem">
+            <Avatar />
+            <section>
+              <div className="commentItem-box1">
+                <h2>{comment.name}</h2>
+                <p>{comment.email}</p>
+              </div>
+              <p>{comment.message}</p>
+            </section>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const Main = (props) => {
+  const { Title, Author, Intro } = props.post;
+  const Comment = props.comment;
+
+  //States
+  const [switchComment, setSwitchComment] = useState(false);
+
+  //Function to toggle addComment Form
+  const toggleComment = () => {
+    if (switchComment === false) {
+      setSwitchComment(true);
+    } else {
+      setSwitchComment(false);
+    }
+  };
+
   return (
     <>
       <section className="postContainer">
-        <h1>
+        <h1 className="postContainer-title">
           {Title}
           <span>{Author}</span>
         </h1>
         {defaultImage && <img src={defaultImage} alt="post" />}
 
-        <p>{Intro}</p>
+        <section className="postContainer-message">
+          <p>{Intro}</p>
+        </section>
       </section>
 
       <section className="commentContainer">
         <h3>Comments</h3>
+        <button className="commentBtn" onClick={toggleComment}>
+          Post a comment
+        </button>
+        {switchComment && (
+          <>
+            <AddComment />
+          </>
+        )}
+
+        <Comments comment={Comment} />
       </section>
     </>
   );
@@ -32,7 +116,7 @@ const Post = () => {
   return (
     <>
       <Header />
-      <Main {...Data[0]} />
+      <Main post={Data[0]} comment={commentData} />
       <Footer />
     </>
   );
---
+++ src/Data/comment.js
@@ -0,0 +1,28 @@
+const commentData = [
+  {
+    id: 1,
+    name: "Eniola",
+    email: "eniola@gmail.com",
+    message: "nice post, keep it up",
+  },
+  {
+    id: 2,
+    name: "Kehinde",
+    email: "Kehinde@gmail.com",
+    message: "nice work, great explanation",
+  },
+  {
+    id: 3,
+    name: "Tolani",
+    email: "tolani@gmail.com",
+    message: "Impressive writeup",
+  },
+  {
+    id: 4,
+    name: "Ayapere",
+    email: "ayapere@gmail.com",
+    message: "Let's go guys",
+  },
+];
+
+export default commentData;
