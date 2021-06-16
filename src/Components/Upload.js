e668de9c3c4efd4ff226049debe398a7e594d21d
Kehinde <adaeza.dev@gmail.com>
2021-06-16 16:27:21 +0100

    currently working on making the upload section functional, also after that, the routes need to be created

--- src/Components/Upload.js
+++ src/Components/Upload.js
@@ -1,6 +1,15 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
 const Upload = () => {
+  //State for each input field
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [brief, setBrief] = useState("");
+  const [format, setFormat] = useState("");
+  const [docFile, setDocFile] = useState();
+  const [cover, setCover] = useState();
+  const [imgFile, setImgFile] = useState();
+
   const [formatChoice, setFormatChoice] = useState(true);
   const formatType = (e) => {
     if (e.target.value === "image") {
@@ -8,6 +17,27 @@ const Upload = () => {
     } else {
       setFormatChoice(true);
     }
+  };
+
+  const [files, setFiles] = useState({
+    title: "Hello",
+    author: "how",
+    brief: "are",
+    format: "you",
+    docFile: "doing",
+    cover: "today",
+    imgFile: "myfriend",
+  });
+
+  useEffect(() => {
+    setFiles({ ...files, title, author, brief, format });
+  }, [title, author, brief, format]);
+
+  const fetchFile = async (e) => {
+    e.preventDefault();
+    // console.log(files.format, files.docFile);
+    //NOTE: Create error-handling here for uploading post.
+    await axios.post("/server", files);
   };
 
   return (
@@ -19,9 +49,15 @@ const Upload = () => {
           name="Title"
           placeholder="Enter article title here..."
           required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <label htmlFor="Author">Author</label>
-        <select name="Author">
+        <select
+          name="Author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
           <option>Author</option>
           <option value="Eniola">Eniola</option>
           <option value="Piere">Piere</option>
@@ -33,9 +69,17 @@ const Upload = () => {
           id="brief"
           placeholder="write a short description here..."
           required
+          value={brief}
+          onChange={(e) => setBrief(e.target.value)}
         ></textarea>
         <label htmlFor="Format">Select Format</label>
-        <select name="format" onClick={(e) => formatType(e)}>
+        <select
+          name="format"
+          onClick={(e) => formatType(e)}
+          value={format}
+          onChange={(e) => setFormat(e.target.value)}
+        >
+          <option>Pick format</option>
           <option value="doc">Doc</option>
           <option value="image">Image</option>
         </select>
@@ -46,6 +90,8 @@ const Upload = () => {
               name="doc"
               accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
               required
+              value={docFile}
+              onChange={(e) => console.log(e)}
             />
             <label htmlFor="cover">Cover(optional)</label>
             <input type="file" name="cover" accept=".jpg, .jpeg, .png" />
@@ -53,7 +99,7 @@ const Upload = () => {
         ) : (
           <input type="file" name="image" accept=".jpg, .jpeg, .png" required />
         )}
-        <input type="submit" value="Post" />
+        <input type="submit" value="Post" onClick={fetchFile} />
       </form>
     </>
   );
